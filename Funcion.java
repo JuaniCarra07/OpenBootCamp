e1c669d934e2b81890948f5403da926b410ef27c
JuaniCarra07 <111442533+JuaniCarra07@users.noreply.github.com>
2022-08-19 11:39:35 -0400

    Add files via upload

---
+++ Funcion.java
@@ -0,0 +1,18 @@
+package Suma;
+
+
+public class Funcion {
+
+  public static void main(String[] args) {
+      int resultado;
+      resultado = suma(40, 30);
+
+    System.out.println(resultado);
+  }
+
+
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+
+}
